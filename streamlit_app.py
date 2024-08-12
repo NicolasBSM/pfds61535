3c427852d0fe227dbd2cfe53625ccafbca5d11ef
NicolasBSM <64396419+NicolasBSM@users.noreply.github.com>
2024-08-12 22:24:54 +0000

    commit1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Proyecto Final Data Science 61535 - Nicolas Ballestero")
+
+st.header("Introduzca sus datos:")
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
