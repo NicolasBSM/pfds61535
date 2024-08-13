f224397ad76b00d1bd7df4dcfb2dfbcbd12ef1a8
NicolasBSM <64396419+NicolasBSM@users.noreply.github.com>
2024-08-13 02:45:32 +0000

    com2

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,10 @@ st.title("Proyecto Final Data Science 61535 - Nicolas Ballestero")
 
 st.header("Introduzca sus datos:")
 
+1=st.selectbox(
+    'tipo de ingreso:',['hola', 'chau']
+)
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
